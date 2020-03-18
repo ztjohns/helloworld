2c5a4f39c104aef2a6a39e478b72aa23b2ffd7fc
Zachary Johnson <zachary.johnson@interclypse.com>
2020-03-18 15:47:15 -0400

    emblem

--- app/components/my-component/component.js
+++ app/components/my-component/component.js
@@ -1,14 +1,14 @@
 import Component from '@ember/component';
 import { tracked } from '@glimmer/tracking';
 import { computed } from '@ember/object';
-
 export default Component.extend({
     firstName: tracked(),
-    lastName: null,
-  
-    // This computed property _implies_ that `firstName` and `lastName`
-    // exist, but we don't know that without reading it.
-    fullName: computed('lastName', function() {
+    lastName: tracked(),
+    fullName: computed('lastName', 'firstName', function() {
       return `${this.firstName} ${this.lastName}`;
-    })
+    }),
+    get afullname() {
+        return `${this.firstName} ${this.lastName}`
+    }
+
   });
